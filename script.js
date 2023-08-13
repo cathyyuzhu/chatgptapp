f3b67b11cae236f734fc89648221a32a2b2d8f74
cathyyuzhu <142132483+cathyyuzhu@users.noreply.github.com>
2023-08-13 12:39:22 +0800

    改key

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = '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';
+
+const userInput = document.getElementById('userInput');
+const submitButton = document.getElementById('submitButton');
+const responseDiv = document.getElementById('response');
+
+submitButton.addEventListener('click', async () => {
+    const userMessage = userInput.value;
+    const response = await getChatGPTResponse(userMessage);
+    displayResponse(response.choices[0].text);
+});
+
+async function getChatGPTResponse(message) {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: message }]
+        })
+    });
+
+    return response.json();
+}
+
+function displayResponse(responseText) {
+    responseDiv.innerHTML = responseText;
+}
